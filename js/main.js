768542c1183eaa6934fa76b448376dd7674cf0b0
SuNiL-Chau <1234huts@gmail.com>
2021-01-21 20:41:04 +0530

    es6 approach update note: uncomment code to check

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@ let img = document.querySelector("#img-swap"),
   bd = document.querySelector("body");
 
 function theme1() {
-    img.src = './assets/img-1.jpg'
-    bd.style.backgroundColor = '#a8d0cf';
+  img.src = "./assets/img-1.jpg";
+  bd.style.backgroundColor = "#a8d0cf";
 }
 
 function theme2() {
-    img.src='./assets/img-2.jpg'
-    bd.style.backgroundColor = "#b3729c";
+  img.src = "./assets/img-2.jpg";
+  bd.style.backgroundColor = "#b3729c";
 }
 
 function theme3() {
-    img.src='./assets/img-3.jpg'
-    bd.style.backgroundColor = "#ed5d45";
+  img.src = "./assets/img-3.jpg";
+  bd.style.backgroundColor = "#ed5d45";
 }
+
+//   es6 approach
+// function theme(val) {
+//   img.src = `./assets/img-${val}.jpg`;
+//   val == 1
+//     ? (bd.style.backgroundColor = "#a8d0cf")
+//     : console.log("opps cannot get body color1");
+//   val == 2
+//     ? (bd.style.backgroundColor = "#b3729c")
+//     : console.log("opps cannot get body color2");
+//   val == 3
+//     ? (bd.style.backgroundColor = "#ed5d45")
+//     : console.log("opps cannot get body color3");
+// }
